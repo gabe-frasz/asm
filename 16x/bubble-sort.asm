85c766a36845e59f8273b12135a7da618eba8ec5
Gabriel Frasão <gabrielvitor.frasao@gmail.com>
2024-11-09 18:24:41 -0300

    make algorithm closer to original

--- 16x/bubble-sort.asm
+++ 16x/bubble-sort.asm
@@ -1,77 +1,49 @@
 .model small
 .data
-  enter db 0ah, 0dh, "$"
-  msg1 db "Enter the array elements: $"
-  msg2 db "Sorted array: $"
-  is_sorted db ?
-  arr db ?
+  enter db 0ah, 0dh, "$" ; newline, carriage return, end of string
+  arr db "988967620$"
+  n dw 9h  
+  is_sorted db 1h
 .code
-  PRINTS MACRO str
-    LEA dx, str
-    MOV ah, 09h
-    INT 21h
-  ENDM
-
-  PRINTC MACRO char
-    MOV dl, char
-    MOV ah, 02h
-    INT 21h
-  ENDM
-
-  SCANC MACRO
-    MOV ah, 01h
-    INT 21h
+  PRINTS MACRO str  ; prints a string
+    LEA dx, str     ; load the address of the string into DX
+    MOV ah, 09h     ; print string function
+    INT 21h        
   ENDM
 
   MOV ax, @data ; set DS to point to the data segment
-  MOV ds, ax    ; DS cannot be accessed directly
-
-  PRINTS msg1
+  MOV ds, ax    ; DS cannot be accessed directly    
+  
+  MOV bx, 0h
+BUBBLE_LOOP:
   MOV si, 0h
-GET_ARRAY_LOOP:
-  SCANC
-  MOV arr[si], al
-  CMP al, "$"
-  JE SORT
-  INC si
-  JMP GET_ARRAY_LOOP
-
-SORT:
-  MOV si, 0h
-  MOV is_sorted, 1h
-SORT_LOOP:
-  MOV bh, arr[si+1]
-  CMP bh, "$"
-  JE CHECK_IF_SORTED
-  MOV bh, arr[si+1]
-  CMP arr[si], bh
-  JG SORT_SWAP
-END_SORT_LOOP:
-  INC si
-  JMP SORT_LOOP
-
-CHECK_IF_SORTED:
-  CMP is_sorted, 1h
-  JE PRINT_ARRAY
-  JNE SORT
-
-SORT_SWAP:
+SWAP_LOOP:   
+  MOV ax, n
+  SUB ax, bx
+  SUB ax, 1h
+  CMP si, ax
+  JGE PRINT_AND_CHECK
   MOV cl, arr[si]
-  MOV bh, arr[si+1]
-  MOV arr[si], bh
+  CMP cl, arr[si+1]
+  JLE SWAP_LOOP_END
+  MOV ah, arr[si+1]
+  MOV arr[si], ah
   MOV arr[si+1], cl
   MOV is_sorted, 0h
-  JMP END_SORT_LOOP
-
-PRINT_ARRAY:
-  PRINTS enter
-  PRINTS msg2
-  MOV si, 0h
-PRINT_ARRAY_LOOP:
-  PRINTC arr[si]
+SWAP_LOOP_END:
   INC si
-  CMP arr[si], "$"
-  JNE PRINT_ARRAY_LOOP
+  JMP SWAP_LOOP
+
+PRINT_AND_CHECK:
+  PRINTS arr
+  PRINTS enter
+  CMP is_sorted, 1h     
+  JE EXIT       
+  MOV is_sorted, 1h
+  INC bx
+  CMP bx, n
+  JGE EXIT
+  JMP BUBBLE_LOOP     
 
 EXIT:
   MOV ah, 4ch ; return control to OS
