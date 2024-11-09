e968ff6a7d25f3e6df8bbf5b39e895ae0183d053
Gabriel Frasão <gabrielvitor.frasao@gmail.com>
2024-11-09 18:58:32 -0300

    make algorithm closer to original

--- 16x/insertion-sort.asm
+++ 16x/insertion-sort.asm
@@ -1,90 +1,40 @@
 .model small
 .data
   enter db 0ah, 0dh, "$"
-  msg1 db "Enter the array elements: $"
-  msg2 db "Sorted array: $"
-  is_sorted db ?
+  arr db "87253445$"
   key db ?
-  arr db ?
 .code
   PRINTS MACRO str
     LEA dx, str
     MOV ah, 09h
     INT 21h
   ENDM
-
-  PRINTC MACRO char
-    MOV dl, char
-    MOV ah, 02h
-    INT 21h
-  ENDM
-
-  SCANC MACRO
-    MOV ah, 01h
-    INT 21h
-  ENDM
-
+ 
   MOV ax, @data ; set DS to point to the data segment
   MOV ds, ax    ; DS cannot be accessed directly
-
-  PRINTS msg1
-  MOV si, 0h
-GET_ARRAY_LOOP:
-  SCANC
-  MOV arr[si], al
-  CMP al, "$"
-  JE INSERTION_SORT
-  INC si
-  JMP GET_ARRAY_LOOP
-
-INSERTION_LOOP:
-  MOV is_sorted, 1h
+ 
   MOV si, 01h
-INSERTION_SORT:
+INSERTION_LOOP:
   MOV cl, arr[si]
   CMP cl, "$"
-  JE CHECK_IF_SORTED
-  MOV cl, arr[si]
+  JE EXIT
   MOV key, cl
   MOV bx, si
 COMPARE:
-  MOV cl, key
   CMP cl, arr[bx-1]
-  JL MAKE_ROOM
-  JMP INSERT_KEY
-END_INSERTION_LOOP:
-  INC si
-  JMP INSERTION_SORT
-
-CHECK_IF_SORTED:
-  CMP is_sorted, 1h
-  JE PRINT_ARRAY
-  JMP INSERTION_LOOP
-
-MAKE_ROOM:
-  MOV is_sorted, 0h
+  JGE INSERTION_LOOP_END
   MOV ah, arr[bx-1]
   MOV arr[bx], ah
   DEC bx
   CMP bx, 0h
-  JE INSERT_KEY
-  JMP COMPARE
-
-INSERT_KEY:
-  MOV cl, key
+  JNE COMPARE
+INSERTION_LOOP_END:
   MOV arr[bx], cl
-  JMP END_INSERTION_LOOP
-
-PRINT_ARRAY:
+  PRINTS arr
   PRINTS enter
-  PRINTS msg2 
-  MOV si, 0h
-PRINT_ARRAY_LOOP:
-  PRINTC arr[si]
   INC si
-  CMP arr[si], "$"
-  JNE PRINT_ARRAY_LOOP
-
+  JMP INSERTION_LOOP
+ 
 EXIT:
   MOV ah, 4ch ; return control to OS
   INT 21h
